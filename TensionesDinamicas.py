c4b295187f60a637bbd102547b0326e98444e992
Tomas Peralta <tomasperalta1099@gmail.com>
2023-05-18 20:14:45 -0300

    Version ALPHA 1.0 Se crean las funciones para los calculos de manera segmentada.

--- TensionesDinamicas.py
+++ TensionesDinamicas.py
@@ -18,43 +18,78 @@ J = 23000 #Momento de inercia polar [mm4]
 g = 9810 #Fuerza de gravedad  [mm/s^2]
 
 #Definicion de variables internas
-f = 0 #Fuerza resultante [Kgf]
-dt = 0 #Deformacion completa [mm]
 fc1 = 101.971 #Factor de conversion de GPa a Kgf/mm2
 G = (E*fc1) / (2*(1+v)) #Modulo de elasticidad Transversal [kgf/mm2]
 
-#Calculos auxiliares
-k1 = ((E*fc1) * A)/l #Rigidez Axial de una barra
-k2 = ((E*fc1) * I )/l #Rigidez flexional de una barra
-k3 = (12 * (E*fc1) * I)/ (l**3) #Rigiez frente a un esfuerzo cortante de una barra
-k4 = (6 * (E*fc1) * I)/ (l**2) #Rigiez frente a un esfuerzo flexo-cortante de una barra
-k5 = (G * J) / l #Rigiez frente a un esfuerzo torsional de una barra
-v = (h * 2 * g)**(1/2) #Velocidad de caida [mm/s]
+
+def tension_dinamica_axial(w, h, A, l, E, fc1):
+
+    k1 = ((E*fc1) * A)/l #Rigidez Axial de una barra
+
+    #Tension dinamica axial
+    f1 = w + (w * (( 1 + ((2 * h * k1)/w) )**(1/2)))
+    sig1 = f1 / A
+
+    print("Tension dinamica AXIAL es: ", round(sig1, 2), " kg/mm^2") 
+
+    return f1, sig1
 
 
-#Metodo de calculo 1 (Shingley)
-#Tension dinamica axial
-f1 = w + (w * (( 1 + ((2 * h * k1)/w) )**(1/2)))
-sig1 = f1 / A 
+def tension_dinamica_flexion(w, h, A, l, E, fc1):
 
-#Tension dinamica en flexion
-f2 = w + (w * (( 1 + ((2 * h * k2)/w) )**(1/2)))
-sig2 = f2 / A 
+    k2 = ((E*fc1) * I )/l #Rigidez flexional de una barra
 
-#Tension dinamica cortante
-f3 = w + (w * (( 1 + ((2 * h * k3)/w) )**(1/2)))
-sig3 = f3 / A 
+    #Tension dinamica en flexion
+    f2 = w + (w * (( 1 + ((2 * h * k2)/w) )**(1/2)))
+    sig2 = f2 / A
 
-#Tension dinamica flexo-cortante
-f4 = w + (w * (( 1 + ((2 * h * k4)/w) )**(1/2)))
-sig4 = f4 / A 
+    print("Tension dinamica FLEXION es: ", round(sig2, 2), " kg/mm^2")
 
-#Tension dinamica en torsion
-f5 = w + (w * (( 1 + ((2 * h * k5)/w) )**(1/2)))
-sig5 = f5 / A 
+    return f2, sig2
 
-print("Tension dinamica AXIAL es: ", round(sig1, 2), " kg/mm^2")
-print("Tension dinamica FLEXION es: ", round(sig2, 2), " kg/mm^2")
-print("Tension dinamica CORTE es: ", round(sig3, 2), " kg/mm^2")
-print("Tension dinamica FLEXO-CORTANTE es: ", round(sig4, 2), " kg/mm^2")
-print("Tension dinamica TORSION es: ", round(sig5, 2), " kg/mm^2")
+def tension_dinamica_corte(w, h, A, l, E, fc1):
+
+    k3 = (12 * (E*fc1) * I)/ (l**3) #Rigiez frente a un esfuerzo cortante de una barra
+
+    #Tension dinamica cortante
+    f3 = w + (w * (( 1 + ((2 * h * k3)/w) )**(1/2)))
+    sig3 = f3 / A
+
+    print("Tension dinamica CORTE es: ", round(sig3, 2), " kg/mm^2")
+
+    return f3, sig3
+
+def tension_dinamica_flexocortante(w, h, A, l, E, fc1):
+
+    k4 = (6 * (E*fc1) * I)/ (l**2) #Rigiez frente a un esfuerzo flexo-cortante de una barra
+    
+    #Tension dinamica flexo-cortante
+    f4 = w + (w * (( 1 + ((2 * h * k4)/w) )**(1/2)))
+    sig4 = f4 / A
+
+    print("Tension dinamica FLEXO-CORTANTE es: ", round(sig4, 2), " kg/mm^2")
+
+    return f4, sig4
+
+def tension_dinamica_torsional(w, h, A, l, G, J):
+
+    k5 = (G * J) / l #Rigiez frente a un esfuerzo torsional de una barra
+
+    #Tension dinamica en torsion
+    f5 = w + (w * (( 1 + ((2 * h * k5)/w) )**(1/2)))
+    sig5 = f5 / A 
+
+    print("Tension dinamica TORSION es: ", round(sig5, 2), " kg/mm^2")
+
+    return f5, sig5
+
+def calc_all(w, h, A, l, E, fc1, G, J):
+
+    f1, sig1 = tension_dinamica_axial(w, h, A, l, E, fc1)
+    f2, sig2 = tension_dinamica_flexion(w, h, A, l, E, fc1)
+    f3, sig3 = tension_dinamica_corte(w, h, A, l, E, fc1)
+    f4, sig4 = tension_dinamica_flexocortante(w, h, A, l, E, fc1)
+    f5, sig5 = tension_dinamica_torsional(w, h, A, l, G, J)
+
+
+calc_all(w, h, A, l, E, fc1, G, J)
