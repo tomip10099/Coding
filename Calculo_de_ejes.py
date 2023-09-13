3505822d686dc3f55dd4c4eb2aab84eed558754d
Tomas Peralta <tomasperalta1099@gmail.com>
2023-09-12 22:21:46 -0300

    Modificaciones

--- Calculo_de_ejes.py
+++ Calculo_de_ejes.py
@@ -108,7 +108,6 @@ B = ((4 * ((kf*Mm2)**2)) + (3 * ((kfs * Tm2)**2)))**(1/2)
 d_gerber = 25.4 * ((((8 * n * A) / (np.pi * Se2)) * (1 + ((1 + (((2 * B * Se2) / (A * Su2))**2))**(1/2))))**(1/3)) # [mm]
 n_gerber = 1 / (((8 * A) / (np.pi * (d2**3) * Se2)) * (1 + ((1 + (((2 * B * Se2)/(A * Su2))**2))**(1/2))))
 
-
 #Metodo de calculo ASME-Elliptic
 
 d_ASME = 25.4 * ((((16*n)/np.pi) * (((4*(((kf*Ma2) / Se2)**2)) + (3 * (((kfs * Ta2) / Se2)**2)) + (4 * (((kf*Mm2) / Sy2)**2)) + (3 * (((kfs * Tm2) / Sy2)**2)))**(1/2)))**(1/3)) # [mm]
@@ -119,10 +118,14 @@ n_ASME = 1 / ((16 / (np.pi * (d2**3))) * (((4 * (((kf*Ma2) / Se2)**2)) + (3 * ((
 d_soderberg = 25.4 * (((16 * n / np.pi) * (((1/Se2) * (((4 * ((kf*Ma2)**2)) + (3 * ((kfs * Ta2)**2)))**(1/2))) + ((1/Sy2) * (((4 * ((kf*Mm2)**2)) + (3 * ((kfs * Tm2)**2)))**(1/2)))))**(1/3)) # [mm]
 n_soderberg =  1 / ((16 / (np.pi * (d2**3))) * (((1/Se2) * (((4 * ((kf*Ma2)**2)) + (3 * ((kfs * Ta2)**2)))**(1/2))) + ((1/Sy2) * (((4 * ((kf*Mm2)**2)) + (3 * ((kfs * Tm2)**2)))**(1/2)))))
 
+#Calculo de tension maxima
+S_max = (((((32 * kf * (Mm2 + Ma2))/(np.pi * (d2**3)))**2) + (3 * (((16 * kfs * (Ta2 + Tm2))/(np.pi * (d2**3)))**2)))**(1/2)) * 0.000703069 #Tension maxima generada al primer ciclo de carga
+n_y = Sy / S_max #Coeficiente de seguridad de tension maxima de fluencia
+
 print("Calculo de ejes")
 print("Parametros del problema: \n","Diametro Menor: ", d, " mm \n", "Diametro Mayor: ", D, " mm \n","Radio de empalme: ", r, " mm\n" )
-print("Caracteristicas del material: \n", "Su: ", Su, round(Su2,3), " kg/mm2(kpsi)\n", "Sy: ", Sy, round(Sy2,3), " kg/mm2(kpsi)\n", "Se: ", round(Se, 3), round(Se2, 3), " kg/mm2(kpsi)\n", sep=" ")
-print("Cocnentracion de tensiones: \n", "kt y kts: ", kt, kts, sep=" ")
+print("Caracteristicas del material: \n", "Su: ", Su, round(Su2,3), " kg/mm2 (kpsi)\n", "Sy: ", Sy, round(Sy2,3), " kg/mm2 (kpsi)\n", "Se: ", round(Se, 3), round(Se2, 3), " kg/mm2 (kpsi)\n", sep=" ")
+print("Cocnentracion de tensiones: \n", "kt y kts: ", kt, kts, sep="")
 print("\n q y qs: ", round(q, 3), round(qs, 3), sep=" ")
 print("\n kf y kfs: ", round(kf, 3), round(kfs, 3), sep=" ")
 
@@ -137,3 +140,6 @@ print("\n Coeficiente de seguridad por ASME: ", round(n_ASME, 2))
 
 print("\n Diametro minimo por Soderber: ", round(d_soderberg, 3))
 print("\n Coeficiente de seguridad por Soderberg: ", round(n_soderberg, 2))
+
+print("\n Tension maxima al primer ciclo: ", round(S_max, 3),round(S_max * 1.42334, 3), "kg/mm2 (kpsi)", sep=" ")
+print("\n Coeficiente de seguridad de maxima carga: ", round(n_y, 2))
